9468f5ab8f4613670355b51d08474b4c75870eae
Ojediran Lawrence <damilareojediran3@gmail.com>
2022-08-25 00:21:04 +0100

    kk

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -21,7 +21,7 @@ function Home() {
     fetchLists();
     var el: HTMLElement = document.querySelector("#lists");
     var sortable = new Sortable.Sortable(el, {
-      delay: 1000,
+      delay: 500,
       delayOnTouchOnly: true,
       disabled: false,
       animation: 100
